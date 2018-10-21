db5a6b6cb41509071045b8bd29bf1aaa4bff5e74
Rhys <rhysrichmond1@gmail.com>
2018-10-21 22:59:02 +0100

    Started the random generation script for the asteroid spawning

---
+++ Assets/Scripts/Enviroment/SpawnAsteroids.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAsteroids : MonoBehaviour {
+
+    public Vector2 center;
+    public Vector2 size;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void RandomAsteroids()
+    {
+        Vector2 pos = center + new Vector2(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.x / 2, size.x / 2));
+    }
+
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(center, size);
+    }
+}
