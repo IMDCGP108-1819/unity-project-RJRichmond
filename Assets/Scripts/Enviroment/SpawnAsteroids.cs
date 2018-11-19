39660946cc56c03c06e0fbb9b31e7ec64a96a1d1
Rhys <rhysrichmond1@gmail.com>
2018-11-19 23:15:58 +0000

    Changed asteroid spawning to use a list of gameobjects to save performance!

--- Assets/Scripts/Enviroment/SpawnAsteroids.cs
+++ Assets/Scripts/Enviroment/SpawnAsteroids.cs
@@ -8,9 +8,29 @@ public class SpawnAsteroids : MonoBehaviour {
     public Vector2 size;
     public GameObject Asteroid;
     public int AsteroidCount;
-
+	public GameObject [] Asteroids;
+	public bool AsteroidAvaliable = true;
+	public int AmountOfAsteroidsActive = 0;
+	public int AllAsteroids =5;
+	
+	void Start(){
+		for(int i = 0; i < AllAsteroids; i++){
+			Asteroids[i] = Instantiate(Asteroid);
+			Asteroids[i].SetActive(false);
+		}
+	}
+	
+	private GameObject NewAsteroid(){
+		for (int i = 0; i < AllAsteroids; i++){
+			if (!Asteroids[i].activeSelf){
+				return Asteroids[i];
+			}
+		}
+		return null;
+	}
+	
     public void FixedUpdate() {
-        if (AsteroidCount < 5) {
+        if (AsteroidCount <= 5) {
             AsteroidCount += 1;
             RandomAsteroids();
         }
@@ -19,11 +39,17 @@ public class SpawnAsteroids : MonoBehaviour {
     //This is the function which chooses a random location based on the variables of size and center, it then spawns a prefab being the asteroid.
     public void RandomAsteroids()
     {
-        Vector2 pos = center + new Vector2(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.x / 2, size.x / 2));
-        Instantiate(Asteroid, pos, Quaternion.identity);
+		Vector2 pos = center + new Vector2(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.x / 2, size.x / 2));
+		GameObject AnAsteroid = NewAsteroid();
+		if (AnAsteroid != null){
+			AnAsteroid.transform.SetPositionAndRotation(pos,Quaternion.identity);
+			AnAsteroid.SetActive(true);
+		}
+        //
+        //Instantiate(Asteroid, pos, Quaternion.identity);
     }
 
-    //This is something which is only avaliable in the editor and it is used to show location of the size and center of the area.
+    //This is something which is only available in the editor and it is used to show location of the size and center of the area.
     void OnDrawGizmosSelected()
     {
         Gizmos.color = new Color(1, 0, 0, 0.5f);
