b9896df54d46d485a89822929de7ff35dd416118
Rhys <rhysrichmond1@gmail.com>
2018-11-20 10:25:42 +0000

    Changed the enemy behaviour to now go for the player and damge now works with both the player and all types of enemies, now going to get everything ready for the scoring system

--- Assets/Scripts/Enemies/EnemyBehavour.cs
+++ Assets/Scripts/Enemies/EnemyBehavour.cs
@@ -3,46 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBehavour : MonoBehaviour {
-	public int EnemyHealth = 5;
+	public int EnemyHealth = 50;
 	public int MoveSpeed = 2;
     public float LookSpeed = 5f;
-	public Transform Player;
+	public GameObject Player;
 	public GameObject Asteroid;
 	public int MaxDist = 10;
 	public int MinDist = 4;
 	
-	void Start(){
-        Player = GameObject.FindGameObjectWithTag("Player").transform;
+	void OnEnable(){
+        Player = GameObject.FindGameObjectWithTag("Player");
+		EnemyHealth = 50;
 	}
 
     /// 
     /// Here I was having trouble with making the enemy look at the player to be able to move properly so I used a 2D rotation tutorial since lookat doesn't work for 2D.
-    /// (Rotate or Aim Towards Mouse or Object in 2D - Unity [ENG]), I did change it slightly so it would work properly!
+    /// (Rotate or Aim Towards Mouse or Object in 2D - Unity [ENG]), I did change it so it would work properly with what I wanted to do!
     /// 
 
     void FixedUpdate(){
-        Vector2 PlayerPosition = Player.position - transform.position;
+        Vector2 PlayerPosition = Player.transform.position - transform.position;
         float angle = Mathf.Atan2(PlayerPosition.x, PlayerPosition.y) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.back);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, LookSpeed * Time.deltaTime);
 
 
-        if (Vector2.Distance(transform.position, Player.position)>= MinDist){
+        if (Vector2.Distance(transform.position, Player.transform.position)>= MinDist){
 			transform.position += transform.up * MoveSpeed * Time.deltaTime;
 		}
-		EnemyDying();
+		
 	}
 	
 	void EnemyDying(){
-		if (EnemyHealth < 0){
-			Destroy(gameObject);
+			print("Check for repeat");
 			GetComponentInParent<ResetAsteroid>().CurrentEnemies -= 1;
-		}
+			gameObject.SetActive(false);
 	}
+	
 	void OnCollisionEnter2D(Collision2D collision){
 		if (collision.gameObject.tag == "Bullet"){
-			EnemyHealth -= 1;
-			Destroy(collision.gameObject);
+			EnemyHealth -= 100;
+			collision.gameObject.SetActive(false);
+			if (EnemyHealth <= 0){
+			EnemyDying();
+			}
+		}
+		else if (collision.gameObject.tag == "Player"){
+			StartCoroutine(DamagePlayer());
 		}
 	}
+	
+	IEnumerator DamagePlayer(){
+		Player.GetComponent<CharController>().Health -= 10;
+		yield return new WaitForSeconds(2f);
+	}
+	
 }
